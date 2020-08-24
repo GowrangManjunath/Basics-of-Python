55dac439a8a013d0206eb80d8bd413e2d38bfb71
Gowrang K Manjunath <67800696+GowrangManjunath@users.noreply.github.com>
2020-08-24 16:08:36 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,6 @@
+#How to add two numbers in python
+a = int(input("Enter your first number :")) #int/float is the data type ; input is the command used for taking input from user
+b = int(input("Enter your second number:")) #int/float is the data type ; input is the command used for taking input from user
+c = a+b
+print("The Addition of your numbers are", c) #whatever is written between the "" are taken as string and will be printed as it is, c will print the value of c
+# in this code the user will not be able to enter decimal number as the input is for interger data type and not float data type. float refers to decimal numbers. If you want to add decimal numbers use  float instead of int in your code
---
+++ division.py
@@ -0,0 +1,6 @@
+#How to divide two numbers in python
+a1 = int(input("Enter your first number :")) #int is the data type ; input is the command used for taking input from user
+b1 = int(input("Enter your second number:")) #int is the data type ; input is the command used for taking input from user
+c1 = a1/b1
+print("The Addition of your numbers are", c1) #whatever is written between the "" are taken as string and will be printed as it is, c1 will print the value of c
+# in this code the user will not be able to enter decimal number as the input is for interger data type and not float data type. float refers to decimal numbers. If you want to add decimal numbers use  float instead of int in your code
