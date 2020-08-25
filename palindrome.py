90ea3b925804b50d1e5ca86d81b54cab702379c7
Ash124a <aaronrodri12345@gmail.com>
2020-08-25 16:04:09 +0530

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,25 @@
-#this code uses function to check for palindrome of a string
-s = input("Enter your word:")
-def isPalindrome(s): 
-    return s == s[::-1]   #This step revereses the string which is entered by the user and then checks if the entered string and reversed string is same or not
-ans = isPalindrome(s) #Here we are returning the function and the resturned function is checked in the if and else statement
-if ans: 
-    print("Yes it is a plaindrome") 
-else: 
-    print("No it is not a plaindrome") 
+#This program is a simple way to check the entered word is a palindrome
+print("Enter the string")
+str1=str(input())
+rev=reversed(str1)       #Reversed function is used to reverse the string
+if list(str1)==list(rev):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+
+#This program is used to check whether the entered number is palindrome
+print("Enter the number ")
+num=int(input())
+temp=num                #temporary variable is used to store the original value
+b=0
+while(num>0):           #while loop for executing the statements
+    d=num%10            #d is storing the remainder of the entered number
+    b=b*10 + d          #b will store the result of the remainder(d) 
+    num=num//10
+if(temp==b):            #if loop is used to compare the temp and the result(b)
+    print("The number is palindrome")
+else:
+    print("The number is not palindrome")
+
+    
