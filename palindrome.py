2ff7592902952a080d8f84cc7c1eaa55abe88cef
Gowrang K Manjunath <67800696+GowrangManjunath@users.noreply.github.com>
2020-08-25 15:56:47 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,8 @@
+#this code uses function to check for palindrome of a string
 s = input("Enter your word:")
 def isPalindrome(s): 
-    return s == s[::-1] 
-ans = isPalindrome(s) 
+    return s == s[::-1]   #This step revereses the string which is entered by the user and then checks if the entered string and reversed string is same or not
+ans = isPalindrome(s) #Here we are returning the function and the resturned function is checked in the if and else statement
 if ans: 
     print("Yes it is a plaindrome") 
 else: 
