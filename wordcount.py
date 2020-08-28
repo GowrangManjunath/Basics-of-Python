24125e924c28de2819e65aea3e7b4ed85c550984
Gowrang K Manjunath <67800696+GowrangManjunath@users.noreply.github.com>
2020-08-28 16:41:21 +0530

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,4 @@
 #program to find word count in a string
 str1 = input("Enter your string:")
-a = len(str1)
+a = len(str1) #len command is used to determine the lenght of the enterned string
 print(a)
