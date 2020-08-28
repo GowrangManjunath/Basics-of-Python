1a5e113aa16e9618985c783f9081c6c2a7426d85
Gowrang K Manjunath <67800696+GowrangManjunath@users.noreply.github.com>
2020-08-28 16:34:15 +0530

    Add files via upload

---
+++ x7!5.py
@@ -0,0 +1,11 @@
+#program for displaying numbers between 10 and 1000 which are divisble by 7 and are not multiple of 5
+def num1():
+	for i in range(10,1001):
+		if (i%5 !=0) and (i%7 ==0):
+			print(i) #printing numbers
+a = num1() #calling function
+print(a)			
+
+			
+		
+		
