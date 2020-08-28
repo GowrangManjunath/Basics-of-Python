f64efedb4ced58d7f6c4f54b75a9dd1c233b341f
Gowrang K Manjunath <67800696+GowrangManjunath@users.noreply.github.com>
2020-08-28 15:10:04 +0530

    Update emi.py

--- emi.py
+++ emi.py
@@ -5,10 +5,10 @@ r = 0.08    #interest rate
 d = ((p*r*pow(1+r,n))/ (pow (1+r,n-1))) #this is the formula to calculate EMI
 print("the emi for", p ,"is", d)
 #using functions to calcualte emi for 1000000 for a period of two years at 8% interest
-#def emi_payment(): 
-	#emi = (p*r*(1+n)*r)//((1+r)*n-1)
-	#print(emi)
-#p = 1000000
-#n = 2
-#r = 0.08
-#d = emi_payment()
+def emi_payment(): 
+	emi = (p*r*(1+n)*r)//((1+r)*n-1)
+	print(emi)
+p = 1000000
+n = 2
+r = 0.08
+d = emi_payment()
