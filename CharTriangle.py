4b0495efa341b16533966f26365e580104f72110
Ash124a <aaronrodri12345@gmail.com>
2020-08-28 15:20:28 +0530

    Add files via upload

---
+++ CharTriangle.py
@@ -0,0 +1,6 @@
+#Program for taking a input from user as Char and printing the triangle
+rows = str(input())
+for num in range(6):            #It prints 6 times as in the for loop specified
+    for i in range(num):
+        print(rows, end=" ")  
+    print(" ")
